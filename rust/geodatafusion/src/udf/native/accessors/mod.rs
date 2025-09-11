742564b7e0a1f2852fe5763ae7d435b671bf4930
Kyle Barron <kyle@developmentseed.org>
2025-09-11 16:53:46 -0400

    feat: ST_NumInteriorRings

--- rust/geodatafusion/src/udf/native/accessors/mod.rs
+++ rust/geodatafusion/src/udf/native/accessors/mod.rs
@@ -1,9 +1,11 @@
 mod coord_dim;
 mod line_string;
 mod npoints;
+mod num_interior_rings;
 mod point;
 
 pub use coord_dim::{CoordDim, NDims};
 pub use line_string::{EndPoint, StartPoint};
 pub use npoints::NPoints;
+pub use num_interior_rings::NumInteriorRings;
 pub use point::{M, X, Y, Z};
